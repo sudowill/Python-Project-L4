030ee9c078a309fe3076fb5fd296d3e4ecc84d79
Will <91379291+willh4cker@users.noreply.github.com>
2022-12-13 10:35:16 +0000

    Needs Work

---
+++ Task Four/Task 4 Needs Work.py
@@ -0,0 +1,12 @@
+class Solution:
+    def solve(self, s, k):
+        def shift(c):
+            i = ord(c) - ord('a')
+            i += k
+            i %= 26
+            return chr(ord('a') + i)
+
+        return "".join(map(shift, s))
+        
+ob = Solution()
+print(ob.solve("abcd", 20))
