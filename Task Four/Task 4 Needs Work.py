52eb2220b6d65190af36f2b1aa4ec4f733f84d4b
Will <91379291+willh4cker@users.noreply.github.com>
2022-12-15 14:02:19 +0000

    Task 4

--- Task Four/Task 4 Needs Work.py
+++ Task Four/Task 4 Needs Work.py
@@ -1,19 +1,24 @@
-import string
-
-class Solution:
-    def solve(self, s, k):
-        def shift(c):
-            i = ord(c) - ord('a')
-            i += k
-            i %= 26
-            return chr(ord('a') + i)
-
-        return "".join(map(shift, s))
-        
-ob = Solution()
-print(ob.solve("abcd", 20))
-
-
-    if the in solve:
-        print("Found shift")
-        print("Processing  shift")
+letters = "abcdefghijklmnopqrstuvwxyz"
+enc_string = open('messagetwo.txt', 'r')
+enc_file = enc_string.read()
+x = 0
+while x < 26:
+    x = x + 1 
+    stringtodecrypt= enc_file
+    stringtodecrypt= stringtodecrypt.lower()
+    ciphershift=int(x)
+    stringdecrypted=""
+    for character in stringtodecrypt:
+        position = letters.find(character)
+        newposition = position - ciphershift
+        if character in letters:
+            stringdecrypted = stringdecrypted + letters[newposition]
+        else:
+            stringdecrypted = stringdecrypted + character
+            
+    ciphershift=str(ciphershift)
+    print("You used a cipher shift of "+ciphershift)
+    print("=-=-=-=-=-=-=-=-=-=")
+    print("Your decrypted message reads:")
+    print(stringdecrypted)
+    print("\n")
